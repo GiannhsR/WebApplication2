9a01a94e651e43cab5ee7dbfe507e239a31b809f
Yiannis Rak <giannhsrak@hotmail.com>
2023-11-22 17:21:53 +0200

    Readme.md

--- Persistence/Models.cs
+++ Persistence/Models.cs
@@ -1,20 +1,22 @@
-﻿public class Post
+﻿namespace WebApplication2.Persistence;
+
+public class Post
 {
-	public int Id { get; set; }
+    public int Id { get; set; }
     public List<Tag> Tags { get; } = new();
     public List<PostTag> PostTags { get; } = new();
 }
 
 public class Tag
 {
-	public int Id { get; set; }
+    public int Id { get; set; }
     public List<Post> Posts { get; } = new();
     public List<PostTag> PostTags { get; } = new();
 }
 
 public class PostTag
 {
-	public int PostId { get; set; }
-	public int TagId { get; set; }
-	public string SomePayload { get; set; }
+    public int PostId { get; set; }
+    public int TagId { get; set; }
+    public string SomePayload { get; set; }
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # WebApplication2
+
+An example repo for https://github.com/dotnet/efcore/issues/32381
+
+1. git pull https://github.com/GiannhsR/WebApplication2.git 
+2. run `dotnet ef migrations add Initial`
+3. Exception received 
+ ```
+ System.InvalidOperationException: The entity type 'PostTag' requires a primary key to be defined.
+ If you intended to use a keyless entity type, call 'HasNoKey' in 'OnModelCreating'. For more information on keyless entity types, 
+ see https://go.microsoft.com/fwlink/?linkid=2141943
+ ```
